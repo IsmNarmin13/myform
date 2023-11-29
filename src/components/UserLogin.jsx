c55184fde1fd9cac4d7f7d12d2b690b13c93ad29
ismnarmin13 <narminvi@code.edu.az>
2023-11-29 15:12:24 +0400

    commit

--- src/components/UserLogin.jsx
+++ src/components/UserLogin.jsx
@@ -1,26 +1,50 @@
 import React from 'react'
 
 const UserLogin = () => {
+    let a = 0;
     function func(e) {
         e.preventDefault();
     }
-   function checkMail(e) {
-    if (e.target.value.includes('.ru')) {
-        document.querySelector('#one').style.display='none';
+    function checkMail(e) {
+        if (!e.target.value.includes('.ru')) {
+            document.querySelector('#one').style.display = 'block';
+        }
+        if (e.target.value.includes('.ru')) {
+            document.querySelector('#one').style.display = 'none';
+            a++;
+        }
+
     }
-   }
+    function checkPass(e) {
+        if (e.target.value.length < 8) {
+            document.querySelector('#two').style.display = 'block';
+        }
+        if (e.target.value.length >= 8) {
+            document.querySelector('#two').style.display = 'none';
+            a++;
+        }
+    }
+    function button() {
+        if (a > 2) {
+            a = 1;
+            document.querySelector('.a').value = ""
+            document.querySelector('.b').value = ""
+        }
+    }
+
+
     return (
         <div className='frm'>
-            <form onClick={(e)=>func(e)}>
+            <form onClick={(e) => func(e)}>
                 <label >Email</label>
-                <input onKeyUp={checkMail} type="text" placeholder="Your email.." />
+                <input className='a' onKeyUp={checkMail} type="text" placeholder="Your email.." />
                 <p id='one'>warning</p>
 
                 <label >Password</label>
-                <input minLength={8} type="text" placeholder="Your password.." />
+                <input className='b' onKeyUp={checkPass} type="text" placeholder="Your password.." />
                 <p id='two'>warning</p>
-                <button>Login</button>
-            </form>
+                <button onClick={button}>Login</button>
+            </form >
         </div>
     )
 }
